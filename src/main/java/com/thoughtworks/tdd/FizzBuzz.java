0330972b40dce01a328e9360f7bfdadfefa20621
曹桂凤 <demi.cao@oocl.com>
2019-07-12 14:29:34 +0800

    [Demi][feat: implement when studentNumber is 125 & test:  fizzBuzz game say 查无此人！]

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -11,49 +11,28 @@ public class FizzBuzz {
         return studentNumber;
     }
 
+
     public String say(int studentNumber) {
-        if (studentNumber%3==0 &&studentNumber%5==0 && studentNumber%7==0 ){
-            return "fizzbuzzwhizz";
-        } else if (studentNumber%3==0 && studentNumber%5==0 ){
-            return "fizzbuzz";
-        }else if(studentNumber%3==0&studentNumber%7==0) {
-            return "fizzwhizz";
-        } else if (studentNumber%5==0 && studentNumber%7==0 ){
-            return "buzzwhizz";
-        } else if (studentNumber%3==0){
-            return "fizz";
-        }else if(studentNumber%5==0){
-            return "buzz";
-        }else if(studentNumber%7==0){
-            return "whizz";
-        }
-        else {
-            return String .valueOf(studentNumber) ;
-        }
-
+        if (studentNumber>=1&& studentNumber<=120){
+            if (studentNumber%3==0 &&studentNumber%5==0 && studentNumber%7==0 ){
+                return "fizzbuzzwhizz";
+            } else if (studentNumber%3==0 && studentNumber%5==0 ){
+                return "fizzbuzz";
+            } else if (studentNumber%3==0 && studentNumber%7==0 ){
+                return "fizzwhizz";
+            } else if (studentNumber%5==0 && studentNumber%7==0 ){
+                return "buzzwhizz";
+            } else if (studentNumber%3==0){
+                return "fizz";
+            } else if (studentNumber%5==0){
+                return "buzz";
+            } else if (studentNumber%7==0){
+                return "whizz";
+            }else {
+                return String.valueOf(studentNumber);
+            }
+    }else {
+        return "查无此人！";
     }
-
-//    public String say(int studentNumber) {
-//        if (studentNumber>=1&& studentNumber<=120){
-//            if (studentNumber%3==0 &&studentNumber%5==0 && studentNumber%7==0 ){
-//                return "fizzbuzzwhizz";
-//            } else if (studentNumber%3==0 && studentNumber%5==0 ){
-//                return "fizzbuzz";
-//            } else if (studentNumber%3==0 && studentNumber%7==0 ){
-//                return "fizzwhizz";
-//            } else if (studentNumber%5==0 && studentNumber%7==0 ){
-//                return "buzzwhizz";
-//            } else if (studentNumber%3==0){
-//                return "fizz";
-//            } else if (studentNumber%5==0){
-//                return "buzz";
-//            } else if (studentNumber%7==0){
-//                return "whizz";
-//            }else {
-//                return String.valueOf(studentNumber);
-//            }
-//    }else {
-//        return "查无此人！";
-//    }
-//    }
+    }
 }
